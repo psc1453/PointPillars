e25919799cced7fb41652b164727f7d58370cea3
pscpeng <pscpeng@outlook.com>
2023-12-07 01:28:29 +0800

    Change a file to infer.

--- inference.py
+++ inference.py
@@ -1,3 +1,7 @@
+###
+# Abbreviation:
+#   pc: point cloud
+
 import argparse
 import os
 
@@ -10,8 +14,9 @@ from utils import read_points, keep_bbox_from_lidar_range, vis_pc
 
 def point_range_filter(pts, point_range=[0, -39.68, -3, 69.12, 39.68, 1]):
     '''
-    data_dict: dict(pts, gt_bboxes_3d, gt_labels, gt_names, difficulty)
-    point_range: [x1, y1, z1, x2, y2, z2]
+    :param pts: (N x 4) numpy array
+    :param point_range: [x1, y1, z1, x2, y2, z2]
+    :return: Filtered point cloud
     '''
     flag_x_low = pts[:, 0] > point_range[0]
     flag_y_low = pts[:, 1] > point_range[1]
@@ -37,9 +42,12 @@ def main(args):
 
     if not os.path.exists(args.pc_path):
         raise FileNotFoundError
+    # Binary point cloud file to 2D Numpy Array (N_points x 4) where 4 is (x, y, z, r) where r is reflection rate
     pc = read_points(args.pc_path)
+    # Filter points that are out of range
     pc = point_range_filter(pc)
     pc_torch = torch.from_numpy(pc).cuda()
+    print(pc_torch.shape)
 
     model.eval()
     with torch.no_grad():
@@ -55,7 +63,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Configuration Parameters')
     parser.add_argument('--ckpt', default='pretrained/epoch_160.pth', help='your checkpoint for kitti')
-    parser.add_argument('--pc_path', default='kitti/training/velodyne/000100.bin', help='your point cloud path')
+    parser.add_argument('--pc_path', default='kitti/training/velodyne/000772.bin', help='your point cloud path')
     args = parser.parse_args()
 
     main(args)
