75292907389e46695d6be35d4e250c648041d54d
pscpeng <pscpeng@outlook.com>
2023-12-08 01:57:34 +0800

    Add comments for PillarEncoder. (Not finished, only step 1~3 are finished. I need to sleep.)

--- model/components/PillarEncoder.py
+++ model/components/PillarEncoder.py
@@ -7,9 +7,12 @@ class PillarEncoder(nn.Module):
     def __init__(self, voxel_size, point_cloud_range, in_channel, out_channel):
         super().__init__()
         self.out_channel = out_channel
+        # Voxel size x and y
         self.vx, self.vy = voxel_size[0], voxel_size[1]
+        # Center location of first voxel with index (0, 0)
         self.x_offset = voxel_size[0] / 2 + point_cloud_range[0]
         self.y_offset = voxel_size[1] / 2 + point_cloud_range[1]
+        # Voxel grid resolution
         self.x_l = int((point_cloud_range[3] - point_cloud_range[0]) / voxel_size[0])
         self.y_l = int((point_cloud_range[4] - point_cloud_range[1]) / voxel_size[1])
 
@@ -24,26 +27,36 @@ class PillarEncoder(nn.Module):
         :return:  (bs, out_channel, y_l, x_l)
         """
         device = pillars.device
-        # 1. calculate offset to the points center (in each pillar)
-        offset_pt_center = pillars[:, :, :3] - torch.sum(pillars[:, :, :3], dim=1, keepdim=True) / npoints_per_pillar[:,
-                                                                                                   None,
-                                                                                                   None]  # (p1 + p2 + ... + pb, num_points, 3)
+        # 1. Calculate offset of every point to the points center in each pillar
+        # xyz coordinate of each points minus average coordinate
+        offset_point_average_center = pillars[:, :, :3] - torch.sum(pillars[:, :, :3], dim=1,
+                                                                    keepdim=True) / npoints_per_pillar[:, None,
+                                                                                    None]  # (p1 + p2 + ... + pb, num_points, 3)
 
-        # 2. calculate offset to the pillar center
-        x_offset_pi_center = pillars[:, :, :1] - (
-                    coors_batch[:, None, 1:2] * self.vx + self.x_offset)  # (p1 + p2 + ... + pb, num_points, 1)
-        y_offset_pi_center = pillars[:, :, 1:2] - (
-                    coors_batch[:, None, 2:3] * self.vy + self.y_offset)  # (p1 + p2 + ... + pb, num_points, 1)
+        # 2. Calculate offset of every point to the pillar center
+        # x coordinate minus pillar center x coordinate
+        x_offset_pillar_center = pillars[:, :, :1] - (
+                coors_batch[:, None, 1:2] * self.vx + self.x_offset)  # (p1 + p2 + ... + pb, num_points, 1)
+        # y coordinate minus pillar center y coordinate
+        y_offset_pillar_center = pillars[:, :, 1:2] - (
+                coors_batch[:, None, 2:3] * self.vy + self.y_offset)  # (p1 + p2 + ... + pb, num_points, 1)
 
-        # 3. encoder
-        features = torch.cat([pillars, offset_pt_center, x_offset_pi_center, y_offset_pi_center],
+        # 3. Encoder
+        # Concat pillar points values, offset to pillar point average, offset to pillar center along the last dimension
+        #       [Batch_Size x Num_Points x 4] # Point Values (x, y, z, r)
+        #       [Batch_Size x Num_Points x 3] # Offset to Point Average in Pillar (offset_x, offset_y, offset_z)
+        #       [Batch_Size x Num_Points x 1] # Offset to Pillar x Center (offset_x_p)
+        #       [Batch_Size x Num_Points x 1] # Offset to Pillar y Center (offset_y_p)
+        #   =>  [Batch_Size x Num_Points x 9]
+        features = torch.cat([pillars, offset_point_average_center, x_offset_pillar_center, y_offset_pillar_center],
                              dim=-1)  # (p1 + p2 + ... + pb, num_points, 9)
-        features[:, :, 0:1] = x_offset_pi_center  # tmp
-        features[:, :, 1:2] = y_offset_pi_center  # tmp
+        # TODO: This meaningless action is due to a legacy bug, try to remove it and re-train the network.
         # In consitent with mmdet3d.
         # The reason can be referenced to https://github.com/open-mmlab/mmdetection3d/issues/1150
+        features[:, :, 0:1] = x_offset_pillar_center  # tmp
+        features[:, :, 1:2] = y_offset_pillar_center  # tmp
 
-        # 4. find mask for (0, 0, 0) and update the encoded features
+        # 4. Find mask for (0, 0, 0) and update the encoded features
         # a very beautiful implementation
         voxel_ids = torch.arange(0, pillars.size(1)).to(device)  # (num_points, )
         mask = voxel_ids[:, None] < npoints_per_pillar[None, :]  # (num_points, p1 + p2 + ... + pb)
