7309d3ca547ba85e081d180e50e9c7174e0250a2
pscpeng <pscpeng@outlook.com>
2023-12-12 21:17:33 +0800

    Rename iou function.

--- ops/nms/iou.py
+++ ops/nms/iou.py
@@ -4,7 +4,7 @@ from ops.nms.classes import Point, Line, RotatedRectangle
 from ops.nms.overlap import box_overlap
 
 
-def iou_box(box_a: RotatedRectangle, box_b: RotatedRectangle) -> float:
+def iou_two_boxes(box_a: RotatedRectangle, box_b: RotatedRectangle) -> float:
     intersect_area = box_overlap(box_a, box_b)
 
     union_area = box_a.area + box_b.area - intersect_area
@@ -14,10 +14,10 @@ def iou_box(box_a: RotatedRectangle, box_b: RotatedRectangle) -> float:
 
 
 def test():
-    box_1 = RotatedRectangle(Point(-1, -1), Point(1, 1))
-    box_2 = RotatedRectangle(Point(-1, -1), Point(1, 1), math.pi / 4)
-    iou = iou_box(box_1, box_2)
-    print(iou)
+    box_1 = RotatedRectangle(Point(12.8248, -13.4288), Point(14.4717, -9.6939), -0.5242)
+    box_2 = RotatedRectangle(Point(12.8257, -13.4140), Point(14.4805, -9.6871), -0.5248)
+    iou = iou_two_boxes(box_1, box_2)
+    print(f'IoU: {iou}')
 
 
 if __name__ == '__main__':
