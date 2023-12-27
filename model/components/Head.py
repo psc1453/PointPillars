9c239603ca72c31acb453f3ae933f65edf6d0d21
pscpeng <pscpeng@outlook.com>
2023-12-27 18:43:04 +0800

    Change detection head to a single conv without bias.

--- model/components/Head.py
+++ model/components/Head.py
@@ -6,21 +6,19 @@ class Head(nn.Module):
     def __init__(self, in_channel, n_anchors, n_classes):
         super().__init__()
 
-        self.conv_cls = nn.Conv2d(in_channel, n_anchors * n_classes, 1)
-        self.conv_reg = nn.Conv2d(in_channel, n_anchors * 7, 1)
-        self.conv_dir_cls = nn.Conv2d(in_channel, n_anchors * 2, 1)
+        # self.conv_cls = nn.Conv2d(in_channel, n_anchors * n_classes, 1)
+        # self.conv_reg = nn.Conv2d(in_channel, n_anchors * 7, 1)
+        # self.conv_dir_cls = nn.Conv2d(in_channel, n_anchors * 2, 1)
+
+        self.n_anchors = n_anchors
+        self.n_classes = n_classes
+        self.conv_all = nn.Conv2d(in_channel, n_anchors * (n_classes + 7 + 2), 1, bias=False)
 
         # in consitent with mmdet3d
         conv_layer_id = 0
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.normal_(m.weight, mean=0, std=0.01)
-                if conv_layer_id == 0:
-                    prior_prob = 0.01
-                    bias_init = float(-np.log((1 - prior_prob) / prior_prob))
-                    nn.init.constant_(m.bias, bias_init)
-                else:
-                    nn.init.constant_(m.bias, 0)
                 conv_layer_id += 1
 
     def forward(self, x):
@@ -31,7 +29,13 @@ class Head(nn.Module):
               bbox_pred: (bs, n_anchors*7, 248, 216)
               bbox_dir_cls_pred: (bs, n_anchors*2, 248, 216)
         """
-        bbox_cls_pred = self.conv_cls(x)
-        bbox_pred = self.conv_reg(x)
-        bbox_dir_cls_pred = self.conv_dir_cls(x)
+        # bbox_cls_pred = self.conv_cls(x)
+        # bbox_pred = self.conv_reg(x)
+        # bbox_dir_cls_pred = self.conv_dir_cls(x)
+
+        bbox = self.conv_all(x)
+        bbox_cls_pred = bbox[:, :self.n_anchors * self.n_classes]
+        bbox_pred = bbox[:, self.n_anchors * self.n_classes: self.n_anchors * (self.n_classes + 7)]
+        bbox_dir_cls_pred = bbox[:, self.n_anchors * (self.n_classes + 7):]
+
         return bbox_cls_pred, bbox_pred, bbox_dir_cls_pred
